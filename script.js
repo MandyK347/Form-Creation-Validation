595dcbe221abe3a34edd6690dbcbd2bd90d4b7c9
MandyK347 <mandy.nnk@gmail.com>
2024-10-08 11:18:47 +0200

    Updated scrip.js

--- script.js
+++ script.js
@@ -34,13 +34,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Display feedback
         feedbackDiv.style.display = "block"; // Show feedback div
-
-        if (isValid) {
-            // If all validations pass
+         if (isValid) {
             feedbackDiv.textContent = "Registration sucessful!";
             feedbackDiv.style.color = "#28a745"; // Green color for success
         } else {
-            // If there are validation errors
             feedbackDiv.innerHTML = messages.join("<br>"); // Display error messages
             feedbackDiv.style.color ="#dc3545"; // Red color for errors
         }
